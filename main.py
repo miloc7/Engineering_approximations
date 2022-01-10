b4650e8b0e535589cc8727e50e5b9eed39cae498
Milo Cannestra <milo.cannestra@gmail.com>
2022-01-09 19:33:07 -0500

    fixing exponents

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-def sqrt_approximator(sqrt_formula, num, guess, precision, root_value):
+def sqrt_approximator(num, guess, precision, root_value):
     for iteration in range(precision):
         num = sqrt_formula(root_value, num, guess)
     return num
 
 def sqrt_formula(root_value, num, guess):
-    new_approximation = (1/root_value)((root_value(guess) + (num)/(guess^root_value)))
+    new_approximation = float((1/root_value)*(root_value*guess + num/guess**root_value))
     return new_approximation
 
 def smart_guesser(root_value, num):
@@ -12,7 +12,9 @@ def smart_guesser(root_value, num):
     close_guess_reached = False
     if close_guess_reached == False:
         for closest_guess in range(100):
-            if closest_guess ^ root_value >= num:
+            if closest_guess ** root_value >= num:
                 close_guess = closest_guess
                 close_guess_reached = True
     return close_guess
+
+print(sqrt_approximator(10, 7, 5, 2))
